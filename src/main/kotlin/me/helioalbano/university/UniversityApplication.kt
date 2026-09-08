bc6da827fa3ef5ed14cd09ffb9a5d042536d5ace
Helio Albano de Oliveira <helioalb@gmail.com>
2026-09-08 10:39:44 -0300

    Initial commit

---
+++ src/main/kotlin/me/helioalbano/university/UniversityApplication.kt
@@ -0,0 +1,11 @@
+package me.helioalbano.university
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UniversityApplication
+
+fun main(args: Array<String>) {
+	runApplication<UniversityApplication>(*args)
+}
